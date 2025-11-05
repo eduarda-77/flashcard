dcf3cd5017abc8d98011cec572940d7432ce8ab0
eduarda-77 <eduarda.schemin.machado@escola.pr.gov.br>
2025-11-05 01:08:08 +0000

    final

---
+++ perguntas.js
@@ -0,0 +1,79 @@
+criaCartao(
+    'Biologia',
+    'Qual o significado de biologia?',
+    'De origem grega, a palavra “Biologia” significa “estudo da vida” (bio = vida e logos = estudo). Como o próprio nome já diz, é a ciência que se dedica a estudar todas as formas de vida, desde os micro-organismos até os seres mais complexos, por exemplo, o ser humano.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'História',
+    'Quem pintou a Monalisa?',
+    'O quadro Mona Lisa foi pintado por Leonardo da Vinci'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Língua Portuguesa',
+    'Onde se usa a expressão "por quê" (separado e com acento)? ',
+    'No final de frases interrogativas diretas, como em "Ele está chorando por quê?". '
+)
+
+criaCartao(
+    'Matemática',
+    'O que é equação de primeiro grau?',
+    'gualdade matemática que contém uma ou mais incógnitas, que são valores desconhecidos representados por letras.'
+)
+
+criaCartao(
+    'Química',
+    'Como identificar um reagente?',
+    'Os reagentes são encontrados no início do processo, enquanto os produtos são encontrados no final. Observar a mudança de cor, odor, estado físico e outras propriedades também pode ajudar a identificar produtos e reagentes.'
+)
+
+criaCartao(
+    'Sociologia',
+    'O que a sociologia estuda?',
+    'O estudo da sociologia é a ciência que estuda a sociedade, as relações sociais e os fenômenos que ocorrem nela, como cultura, economia, política, educação e desigualdade.'
+)
+criaCartao(
+    'Educação Financeira',
+    'O que é a regra dos 50, 30 e 20?',
+    ' Regra 50-30-20 é uma ferramenta de planejamento financeiro que propõe dividir a renda líquida mensal em três categorias: 50% para necessidades essenciais (moradia, alimentação, transporte), 30% para desejos (lazer, hobbies, restaurantes) e 20% para prioridades financeiras (quitar dívidas e investir).'
+)
+
+criaCartao(
+    'Projeto De Vida',
+    'O que é Projeto de vida?',
+    'é um plano detalhado e pessoal para definir objetivos, aspirações, metas e ações ao longo da vida. '
+)
+
+criaCartao(
+    'Biologia I',
+    'O que se estuda em Biologia 1?',
+    'A Biologia é uma ciência que estuda a vida em seus mais variados aspectos, importando-se em compreender, por exemplo, o funcionamento dos organismos vivos, a relação desses seres com o meio e seu processo de evolução.'
+)
+
+criaCartao(
+    'Fisica 1',
+    'O que é Arduino e para que serve?',
+    'Arduino é uma plataforma de hardware e software de código aberto para prototipagem eletrônica, ideal para iniciantes e avançados. Ele serve para criar projetos interativos, como robôs e sistemas de automação.'
+)
+criaCartao(
+    'Matemática 1',
+    'O que é raciocínio lógico?',
+    'O raciocínio lógico é a habilidade de pensar de forma organizada e sistemática para resolver problemas. '
+)
+criaCartao(
+    'Matemática 2',
+    'o que é HTML',
+    'HTML Linguagem de Marcação de Hipertexto é a linguagem padrão para criar e estruturar páginas da web.'
+)
